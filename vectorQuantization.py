4383217baaa9fd83d54423a578130b8a76eee9f8
Aly-hosny <aly.hosny.37@gmail.com>
2020-04-07 09:48:56 +0200

    faster distance calc, add live plot option

--- vectorQuantization.py
+++ vectorQuantization.py
@@ -31,54 +31,55 @@ class vectorQuant:
         self.outWav = outWav
 
 
-    def createModel(self):
+    def createModel(self,livePlot=False):
         '''
         Create a codeBook model for quantizing based on training file, eps, dim.
         '''
-        reshapedframesTrain = self.framesTrain[:int(len(self.framesTrain.flat)/self.dim[1])*self.dim[1]].reshape(-1,self.dim[1]) # reshape frames based on the codeBook dimensions
+        reshapedframesTrain = self.framesTrain[:int(len(self.framesTrain.flat)/self.dim[1])*self.dim[1]].reshape(-1,self.dim[1])/np.amax(self.framesTrain) # reshape frames based on the codeBook dimensions
 
 
-        self.codeBook = np.random.normal(np.mean(reshapedframesTrain), np.std(reshapedframesTrain), size=(self.dim)) # random initialization for codebook
+        self.codeBook = np.random.uniform(np.amin(reshapedframesTrain), np.amax(reshapedframesTrain), size=(self.dim)) # random initialization for codebook
         # cbIdx = np.argsort(np.sum(iniCodeBook**2,axis=1))
         # self.codeBook = iniCodeBook[cbIdx]
         # dist = np.zeros([np.shape(reshapedframesTrain)[0],np.shape(self.codeBook)[0]]) #empty array to store distance between each vector and all elements in codebook
         teps = 1e10
+        if livePlot: # if live plotting is enabeled prepare the figure
+            import matplotlib.pyplot as plt
+            plt.ion()
+            fig, ax = plt.subplots()
+            ax.scatter(reshapedframesTrain[:,0],reshapedframesTrain[:,1])
+            sp = ax.scatter(self.codeBook[:,0],self.codeBook[:,1])
+            plt.draw()
+
         while teps>self.eps:
-            ### old attempts for ind 'slower'
-            # repeatedCB =  np.rot90(ml.repmat(codeBook,reshapedframesTrain.shape[0],1).reshape(-1,16,2),axes=(1,0))
-            # ind = np.argmin((np.sum((reshapedframesTrain - repeatedCB)**2,axis=2)**0.5) , axis=0)
+            diffToCB =  reshapedframesTrain - np.rot90(ml.repmat(self.codeBook,reshapedframesTrain.shape[0],1).reshape(-1,self.dim[0],self.dim[1]),axes=(1,0)) # subtract each audio frames from all the entries of the codeBook
+            ind = np.argmin(np.sqrt(np.einsum('ijk,ijk->ij', diffToCB, diffToCB)) , axis=0) # get the index of the codebook entry with minumy distance to each audio frame
 
-            repeatedCB =  reshapedframesTrain - np.rot90(ml.repmat(self.codeBook,reshapedframesTrain.shape[0],1).reshape(-1,16,2),axes=(1,0))
-            ind = np.argmin(np.sqrt(np.einsum('ijk,ijk->ij', repeatedCB, repeatedCB)) , axis=0)
-
-            # for i in range(self.dim[0]):
-            #     difference = reshapedframesTrain - ml.repmat(self.codeBook[i,:],np.shape(reshapedframesTrain)[0],1)
-            #     dist[:,i] = np.sqrt(np.sum(difference**2,1)) 
-            # ind = np.argmin(dist,axis=1)
-
-            # ind = np.digitize(np.sum(reshapedframesTrain**2,1) , np.sum(self.codeBook**2,1))
-
-            tempCB = np.array([np.nan_to_num(np.mean(reshapedframesTrain[ind==i],axis=0)) for i in range(self.dim[0])])
-            # tempIdx = np.argsort(np.sum(tempCB**2,axis=1))
-            # tempCB = tempCB[tempIdx]
+            tempCB = np.array([np.nan_to_num(np.mean(reshapedframesTrain[ind==i],axis=0)) for i in range(self.dim[0])]) # create a new codebook with new entries based on the mean value of all frames that belong to a specific entry in the old codebook
             
-            teps= np.sqrt(np.sum((tempCB-self.codeBook)**2))
+            teps= np.sqrt(np.sum((tempCB-self.codeBook)**2)) # get the distance between new and old codebook
             print ('eps is', teps)
-            self.codeBook=tempCB.copy()
-################################################################################## Add plot ##################################################################################
+            self.codeBook=tempCB.copy() # assign the new values to the codeBook
+
+            if livePlot: # upfate plot
+                sp.set_offsets(self.codeBook)
+                fig.canvas.draw_idle()
+                plt.pause(0.0001)
+
+
     def applyQuantization(self,audioFile):
         audioFile = wave.open(audioFile, 'r') 
-        self.samplingRate = audioFile.getframerate()
-        mode = 'stereo' if audioFile.getnchannels()==2 else 'mono'
-        nFrames = audioFile.getnframes()
+        self.samplingRate = audioFile.getframerate() # get the samplingRate of input audio file
+        mode = 'stereo' if audioFile.getnchannels()==2 else 'mono' # get mode
+        nFrames = audioFile.getnframes() # get frames count
         pck = "<2h" if mode == 'stereo' else "<h"
-        frames =  np.array([struct.unpack(pck ,audioFile.readframes(1)) for i in range(nFrames)])
+        frames =  np.array([struct.unpack(pck ,audioFile.readframes(1)) for i in range(nFrames)]) # get frames
      
-        reshapedFrames = frames[:int(len(frames.flat)/self.dim[1])*self.dim[1]].reshape(-1,self.dim[1])
+        reshapedFrames = frames[:int(len(frames.flat)/self.dim[1])*self.dim[1]].reshape(-1,self.dim[1])/np.amax(frames) #reshape frames based on the desired codebook shape
 
-        dist2 = np.zeros([nFrames,self.dim[0]])
-        for i in range(self.dim[0]):
-            difference = reshapedFrames - ml.repmat(self.codeBook[i,:],np.shape(reshapedFrames)[0],1)
-            dist2[:,i] = np.sqrt(np.sum(difference**2,1))
-        self.ind = np.argmin(dist2,axis=1)
-        self.outputDecoded=self.codeBook[self.ind].reshape(frames.shape)
+        # dist2 = np.zeros([nFrames,self.dim[0]])
+
+        diffToCB =  reshapedFrames - np.rot90(ml.repmat(self.codeBook,reshapedFrames.shape[0],1).reshape(-1,self.dim[0],self.dim[1]),axes=(1,0)) # subtract each audio frames from all the entries of the codeBook
+        self.ind = np.argmin(np.sqrt(np.einsum('ijk,ijk->ij', diffToCB, diffToCB)) , axis=0) # get the index of the codebook entry with minmum distance to each audio frame
+
+        self.outputDecoded=self.codeBook[self.ind].reshape(frames.shape) * np.amax(frames)
